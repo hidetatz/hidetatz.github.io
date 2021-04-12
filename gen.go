90d87cce8a89474df109d489b3fe64e8cb6ac2ce
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-04-12 19:01:03 +0900

    split main.go to look better

---
+++ gen.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	cname = "dtyler.io"
+
+	// the number of "recent articles" on 404 page
+	articlesCountOn404Page = 7
+)
+
+var (
+	//go:embed assets/favicon.ico
+	favicon string
+
+	//go:embed assets/markdown.css
+	markdownCSS string
+
+	//go:embed assets/syntax.css
+	syntaxCSS string
+
+	//go:embed assets/highlight.pack.js
+	syntaxJS string
+
+	//go:embed data/about.md
+	about string
+
+	//go:embed data/404.md
+	notFoundPage string
+)
+
+func gen() {
+	// required for GitHub pages
+	write(cname, "./docs/CNAME")
+
+	// assets
+	write(markdownCSS, "./docs/markdown.css")
+	write(syntaxCSS, "./docs/syntax.css")
+	write(syntaxJS, "./docs/syntax.js")
+	write(favicon, "./docs/favicon.ico")
+
+	articles := ReadArticles("./data/articles")
+	articleList := ListArticlesHref(articles)
+
+	for _, a := range articles {
+		// if url != nil, no need to generate the page because it is linked from nowhere
+		if a.URL == nil {
+			write(GenerateArticlePageHTML(a), fmt.Sprintf("./docs/articles/%s/%s/index.html", a.FormatTime(), a.FileNameWithoutExtension()))
+		}
+	}
+
+	articlesJA := ReadArticles("./data/articles/ja")
+	articlesJAList := ListArticlesHref(articlesJA)
+
+	for _, a := range articlesJA {
+		if a.URL == nil {
+			write(GenerateArticlePageHTML(a), fmt.Sprintf("./docs/articles/%s/%s/index.html", a.FormatTime(), a.FileNameWithoutExtension()))
+		}
+	}
+
+	idx := GenerateIndexPageHTML(strings.Join(articleList, "\n"))
+	idxJA := GenerateJaIndexPageHTML(strings.Join(articlesJAList, "\n"))
+
+	write(idx, "./docs/index.html")
+	write(idxJA, "./docs/ja/index.html")
+
+	write(GenerateHTMLPage("about", about), "./docs/about/index.html")
+
+	articlesFor404Page := ""
+	for i := 0; i < articlesCountOn404Page; i++ {
+		articlesFor404Page += articleList[i] + "\n"
+	}
+	write(GenerateHTMLPage("404", fmt.Sprintf(notFoundPage, articlesFor404Page)), "./docs/404.html")
+}
+
+func write(content, fileNameWithDir string) {
+	err := os.MkdirAll(filepath.Dir(fileNameWithDir), 0775)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.OpenFile(fileNameWithDir, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	defer func() {
+		_ = file.Close()
+	}()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func removeAllFiles(dir string) {
+	d, err := os.Open(dir)
+	defer func() {
+		_ = d.Close()
+	}()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileinfo, err := d.Readdir(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, info := range fileinfo {
+		if info.IsDir() {
+			removeAllFiles(filepath.Join(dir, info.Name()))
+		}
+
+		err := os.Remove(filepath.Join(dir, info.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
---
+++ run_server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/atotto/clipboard"
+	"github.com/fsnotify/fsnotify"
+)
+
+func runServer() {
+	ctx := context.Background()
+
+	port := "8080"
+	directory := "./docs"
+	ip := getLocalIP()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	server := &http.Server{Addr: ":8080", Handler: http.FileServer(http.Dir(directory))}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	fmt.Printf("Serving at %s:%s (the address is automatically copied into the clipboard)\n", ip, port)
+	clipboard.WriteAll(fmt.Sprintf("%s:%s", ip, port))
+
+	// live reload with fsnotify
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write != fsnotify.Write {
+					continue
+				}
+
+				fmt.Println("files edited. hot reloading...")
+				server.Shutdown(ctx)
+				time.Sleep(time.Second) // wait for shutdown
+
+				removeAllFiles("./docs/")
+				gen()
+				server = &http.Server{Addr: ":8080", Handler: http.FileServer(http.Dir(directory))}
+				// don't take care of goroutine cancel since this is not a long-running script and
+				// want to make code look easy
+				go func() {
+					server.ListenAndServe()
+				}()
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	watcher.Add("./data/articles/ja")
+	watcher.Add("./data/articles/")
+	<-done
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+
+	return ""
+}
