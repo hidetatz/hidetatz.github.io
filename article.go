098eb5a381a0283837b46ef7a64979fe42789c95
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-04-14 13:28:37 +0900

    add via on the tweet

--- article.go
+++ article.go
@@ -26,7 +26,7 @@ const articlePageMD = `
 
 %s
 
-<a href="https://twitter.com/share?ref_src=twsrc%%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<a href="https://twitter.com/share?ref_src=twsrc%%5Etfw" class="twitter-share-button" data-via="dty1er1" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 ---
 
