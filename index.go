c13623c26f8f71a7d89d6e3f26d2834b13321ec1
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-11 08:15:41 +0900

    deploy

--- index.go
+++ index.go
@@ -5,9 +5,34 @@ import "fmt"
 const indexPageMD = `
 # hidetatz.io
 
-hidetatz.io is my personal blog. The author Hidetatsu (pronounced he-day-tatz) is a software engineer mainly focuses on system architecture, reliability, performance and observability based in Japan. I write code around infrastructure, database, transaction, concurrent programming and distributed systems. My code is available in [GitHub](https://github.com/hidetatz).
+hidetatz.io is my personal website. The author Hidetatz (pronounced he-day-tatz) is a software engineer mainly focuses on system architecture, reliability, performance and observability based in Japan. I write code around infrastructure, database, transaction, concurrent programming and distributed systems. My code is available in [GitHub](https://github.com/hidetatz).
 
 I [do fail](https://hidetatz.fail/).
+
+---
+
+## Projects
+
+Give them a star!
+
+### kubecolor (Go)
+
+https://github.com/hidetatz/kubecolor
+
+kubecolor is a CLI tool which wraps kubectl and colorizes the output for readability.
+You can read my [blog article](https://hidetatz.medium.com/colorize-kubectl-output-by-kubecolor-2c222af3163a) about it.
+
+### collection (Go)
+
+https://github.com/hidetatz/collection
+
+collection is a generics-aware Go library which provides collection data structures like [Java's one](https://docs.oracle.com/javase/8/docs/api/java/util/Collections.html).
+
+### size-limited-queue (Go)
+
+https://github.com/hidetatz/size-limited-queue
+
+size-limited-queue is a blocking queue implementation. Internally sync.Cond is used and I made this repository to [describe](https://hidetatz.io/articles/2021/04/13/sync_cond/) how to use it.
 
 ---
 
