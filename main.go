4e33f38602588dd64bfb12928bd4e9d9ee336339
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-02-14 15:52:37 +0900

    fix 404

--- main.go
+++ main.go
@@ -454,10 +454,10 @@ func gen() {
 	for i := 0; i < articlesCountOn404Page; i++ {
 		articlesFor404Page += fmt.Sprintf("[%s](%s)  \n", articles[i].title, linkToArticle(articles[i]))
 	}
-	write(generateHTMLPage("404 | hidetatz.io", fmt.Sprintf(notFoundPage, articlesFor404Page)), "./docs/404.html")
+	write(convertArticleToHTML("404 | hidetatz.io", fmt.Sprintf(notFoundPage, articlesFor404Page), nil), "./docs/404.html")
 
 	// other writings
-	write(convertArticleToHTML("Learn distributed systems", distsys, nil), "./docs/distsys.html")
+	write(convertArticleToHTML("Learn distributed systems | hidetatz.io", distsys, nil), "./docs/distsys.html")
 }
 
 func main() {
