b8a414bf0b376049aadc6039e49b81f5e8aa0124
Hidetatz Yaginuma in CI <hidetatz@gmail.com>
2024-06-21 10:38:20 +0900

    fix

--- generate.py
+++ generate.py
@@ -314,7 +314,8 @@ class Blog:
             elif os.path.isdir(item):
                 shutil.rmtree(item)
 
-        shutil.move(f"../{self.root}/*", f"./")
+        for file in glob.glob(f"../{self.root}/*")
+            shutil.move(file, f"./")
 
         subprocess.run(["git", "add", "."])
         subprocess.run(["git", "commit", "-m", "update"])
